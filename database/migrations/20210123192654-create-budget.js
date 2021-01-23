9647a342550553746eb329109d3f8c4d163cddbd
Erik Björn <erik.g.bjoern@gmail.com>
2021-01-23 20:33:46 +0100

    create a budget model

---
+++ database/migrations/20210123192654-create-budget.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('budget,s', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      amount: {
+        type: Sequelize.INTEGER
+      },
+      start_date: {
+        type: Sequelize.STRING
+      },
+      end_date: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('budget,s');
+  }
+};
